cd4d6a84affb2f57c44bedbf7dce28a913ef7cd2
fozzy77 <56725755+fozzy77@users.noreply.github.com>
2022-01-11 13:08:17 +0000

    Aligned provider pinning

--- provider.tf
+++ provider.tf
@@ -1,4 +1,17 @@
 provider "aws" {
+  terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.71.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.2.0"
+    }
+  }
+  required_version = "~> 1.1"
+}
   region = local.config.region
 }
   
